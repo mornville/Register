0aef86b1b0022e4d63e496f4de3fe71f2f5d7a29
Ashutosh Jha <aj97389@gmail.com>
2019-06-14 09:50:46 +0530

    splash screen readyh

--- register_app/lib/main.dart
+++ register_app/lib/main.dart
@@ -1,58 +1,105 @@
 import 'package:flutter/material.dart';
-
+import 'dart:async';
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue,),
+      title: 'Register.app',
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _SplashScreenState();
+  }
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 5),(){
+      debugPrint('Stopped  Space');
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(
-      title: 'Register',
-      debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          // Define the default brightness and colors.
-          brightness: Brightness.light,
-          primarySwatch: Colors.red,
-
-
-        ),
-      home: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(300.0), // here the desired height
-          child: AppBar(
-            title: Container(
-              child: Column(
-                children: <Widget>[
-                  Expanded(
-                    child: Center(
-                      child: Text(
-                        'The Register.app',
-                        style: TextStyle(fontFamily: 'Roboto',fontSize: 30.0),
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.blue),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 3,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 60.0,
+                        child: Icon(
+                          Icons.blur_on,
+                          color: Colors.blue,
+                          size: 60.0,
+                        ),
                       ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 40.0),
+                      ),
+                      Text(
+                        'The Register.app',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 36.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                        child: Text('Powered By VeePay',style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w300)),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(
+                      backgroundColor: Colors.yellow,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top:20.0),
+                    ),
+                    Center(
+                      child:Text('Your Daily Transactions Easily, Everyday',style: TextStyle(color: Colors.white,fontWeight: FontWeight.w700,fontSize: 18.0),),
 
                     ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text('Wanna Login?',
-                      style: TextStyle(fontSize: 20.0, color: Colors.black87)),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ),
+        ],
       ),
     );
   }
