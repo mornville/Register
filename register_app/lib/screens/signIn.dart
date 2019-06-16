faf2cd743d8b6419c7b3a2327ce38342d0c17a8a
Ashutosh Jha <aj97389@gmail.com>
2019-06-16 14:34:07 +0530

    Overflow at Sign IN Page fixed

--- register_app/lib/screens/signIn.dart
+++ register_app/lib/screens/signIn.dart
@@ -33,113 +33,107 @@ class _SignInState extends State<SignIn> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
 
-                        Padding(
-                          padding: EdgeInsets.only(top:40.0),
-                          child:CircleAvatar(
-                            backgroundColor: Colors.white,
-                            radius: 40.0,
-                            backgroundImage:
-                            AssetImage("images/user.png"),
-                          ),
-                        ),
                         Expanded(
-                            flex: 1,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Padding(
-                                  padding: EdgeInsets.all(0.0),
-                                  child: Text(
-                                    'Sign In To Register.app',
-                                    style: TextStyle(
-                                        color: Colors.blue,
-                                        fontWeight: FontWeight.w300,
-                                        fontSize: 20.0),
-                                  ),
-                                ),
-                              ],
-                            )),
-                        Expanded(
-                          flex: 5,
+
                           child: Center(
                             child: Padding(
                               padding: EdgeInsets.all(20.0),
-                              child: Column(
+                              child: ListView(
                                 children: <Widget>[
-                                  Flexible(
-                                    child:   //userName
+                                 Column(
+                                   children: <Widget>[
+                                     Padding(
+                                       padding: EdgeInsets.only(top:20.0),
 
-                                    Container(
-                                      width: 300.0,
-                                      child: Padding(
-                                        padding: EdgeInsets.all(20.0),
-                                        child: TextField(
-                                          style: TextStyle(color: Colors.black,
-                                          ),
-                                          decoration: InputDecoration(
-                                            hintText: 'Enter Username Here',
-                                            contentPadding: EdgeInsets.all(20.0),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  //password
+                                         child: CircleAvatar(
+                                           backgroundColor: Colors.white,
+                                           radius: 40.0,
+                                           backgroundImage:
+                                           AssetImage("images/user.png"),
+                                         ),
+                                       ),
 
-                                  Container(
-                                    width: 300.0,
-                                    child: Padding(
-                                      padding: EdgeInsets.all(20.0),
-                                      child: Column(
-                                        children: <Widget>[
-                                          TextField(
-                                            style: TextStyle(color: Colors.black,
-                                            ),
-                                            decoration: InputDecoration(
-                                              hintText: 'Enter Password Here',
-                                              contentPadding: EdgeInsets.all(20.0),
-                                            ),
-                                          ),
-                                          Row(
-                                            mainAxisAlignment: MainAxisAlignment.end,
-                                            children: <Widget>[
-                                              Padding(
-                                                padding: EdgeInsets.only(top:20.0),
-                                                child:Text('Forgot Password?',style: TextStyle(fontWeight: FontWeight.w500),),
-                                              ),
 
-                                            ],
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
+                                     Padding(
+                                       padding: EdgeInsets.all(20.0),
+                                       child: Text(
+                                         'Sign In To Register.app',
+                                         style: TextStyle(
+                                             color: Colors.blue,
+                                             fontWeight: FontWeight.w300,
+                                             fontSize: 20.0),
+                                       ),
+                                     ),
+                                     //password
+                                     Container(
+                                       width: 300.0,
+                                       child: Padding(
+                                         padding: EdgeInsets.all(20.0),
+                                         child: TextField(
+                                           style: TextStyle(color: Colors.black,
+                                           ),
+                                           decoration: InputDecoration(
+                                             hintText: 'Enter Username Here',
+                                             contentPadding: EdgeInsets.all(20.0),
+                                           ),
+                                         ),
+                                       ),
+                                     ),
+                                     Container(
+                                       width: 300.0,
+                                       child: Padding(
+                                         padding: EdgeInsets.all(20.0),
+                                         child: Column(
+                                           children: <Widget>[
+                                             TextField(
+                                               style: TextStyle(color: Colors.black,
+                                               ),
+                                               decoration: InputDecoration(
+                                                 hintText: 'Enter Password Here',
+                                                 contentPadding: EdgeInsets.all(20.0),
+                                               ),
+                                             ),
+                                             Row(
+                                               mainAxisAlignment: MainAxisAlignment.end,
+                                               children: <Widget>[
+                                                 Padding(
+                                                   padding: EdgeInsets.only(top:20.0),
+                                                   child:Text('Forgot Password?',style: TextStyle(fontWeight: FontWeight.w500),),
+                                                 ),
 
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
+                                               ],
+                                             ),
+                                           ],
+                                         ),
+                                       ),
+                                     ),
 
-                                    children: <Widget>[
-                                      Padding(padding: EdgeInsets.all(20.0),
-                                      child: Container(
-                                        width: 100.0,
-                                        height: 40.0,
-                                        child: RaisedButton(
-                                          color: Colors.lightBlue,
-                                          onPressed: () {
-                                            Navigator.of(context).pushNamed('/HomePage');
-                                          },
-                                          child: Text(
-                                            'Sign In',
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 18.0,
-                                                fontWeight: FontWeight.w400),
-                                          ),
-                                        ),
-                                      ),)
-                                    ],
-                                  ),
+                                     Row(
+                                       mainAxisAlignment: MainAxisAlignment.center,
 
+                                       children: <Widget>[
+                                         Padding(padding: EdgeInsets.all(20.0),
+                                           child: Container(
+                                             width: 100.0,
+                                             height: 40.0,
+                                             child: RaisedButton(
+                                               color: Colors.lightBlue,
+                                               onPressed: () {
+                                                 Navigator.of(context).pushNamed('/HomePage');
+                                               },
+                                               child: Text(
+                                                 'Sign In',
+                                                 style: TextStyle(
+                                                     color: Colors.white,
+                                                     fontSize: 18.0,
+                                                     fontWeight: FontWeight.w400),
+                                               ),
+                                             ),
+                                           ),)
+                                       ],
+                                     ),
+                                   ],
+                                 ),
 
                                 ],
                               ),
