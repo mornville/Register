2d1bb42a2ae946088873672ee53d37298385e458
Ashutosh Jha <aj97389@gmail.com>
2019-06-29 23:52:13 +0530

    Added G-signIn Button

--- register_app/lib/screens/signIn.dart
+++ register_app/lib/screens/signIn.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:register_app/screens/auth.dart';
 
 class SignIn extends StatefulWidget {
   @override
@@ -125,6 +126,14 @@ class _SignInState extends State<SignIn> {
                                            ),
                                          ),
 
+                                         Center(
+                                           child: Column(
+                                             children: <Widget>[
+                                               LoginButton(), // <-- Built with StreamBuilder
+                                               UserProfile() // <-- Built with StatefulWidget
+                                             ],
+                                           ),
+                                         ),
                                          Row(
                                            mainAxisAlignment: MainAxisAlignment.center,
 
@@ -168,5 +177,58 @@ class _SignInState extends State<SignIn> {
             ),
           ],
         ));
+  }
+}
+
+
+class UserProfile extends StatefulWidget {
+  @override
+  UserProfileState createState() => UserProfileState();
+}
+
+class UserProfileState extends State<UserProfile> {
+  Map<String, dynamic> _profile;
+  bool _loading = false;
+
+  @override
+  initState() {
+    super.initState();
+
+    // Subscriptions are created here
+    authService.profile.listen((state) => setState(() => _profile = state));
+
+    authService.loading.listen((state) => setState(() => _loading = state));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Container(padding: EdgeInsets.all(20), child: Text(_profile.toString())),
+      Text(_loading.toString())
+    ]);
+  }
+}
+class LoginButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: authService.user,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return MaterialButton(
+              onPressed: () => authService.signOut(),
+              color: Colors.red,
+              textColor: Colors.white,
+              child: Text('Signout'),
+            );
+          } else {
+            return MaterialButton(
+              onPressed: () => authService.googleSignIn(),
+              color: Colors.white,
+              textColor: Colors.black,
+              child: Text('Login with Google'),
+            );
+          }
+        });
   }
 }
